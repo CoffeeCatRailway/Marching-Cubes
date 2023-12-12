f0961936b7839a28c50fba57e56d263d4f8a0251
CoffeeCatRailway <dragonlee1002@gmail.com>
2023-12-13 09:46:07 +1100

    :recycle: Test actor now has fly & walk speeds

--- scripts/TestActor.gd
+++ scripts/TestActor.gd
@@ -1,11 +1,12 @@
 extends CharacterBody3D
 
-@export var speed := 15.
+@export var walkSpeed := 15.
+@export var flySpeed := 60.
 @export var flying := false
 
 var isMoving := false
 var originalPos := Vector3.ZERO
-var vieSensitivity := .3
+@export_range(.1, 1., .05) var viewSensitivity := .3
 var pitch := 0.
 var yaw := 0.
 var mouseModeToggle := true # false is visible
@@ -33,8 +34,8 @@ func _ready() -> void:
 func _input(event) -> void:
 	if event is InputEventMouseMotion:
 		if mouseModeToggle:
-			pitch = max(min(pitch - (event as InputEventMouseMotion).relative.y * vieSensitivity, 90.), -90.)
-			yaw = fmod(yaw - (event as InputEventMouseMotion).relative.x * vieSensitivity, 360.)
+			pitch = max(min(pitch - (event as InputEventMouseMotion).relative.y * viewSensitivity, 90.), -90.)
+			yaw = fmod(yaw - (event as InputEventMouseMotion).relative.x * viewSensitivity, 360.)
 			$Camera3D.rotation.x = deg_to_rad(pitch)
 			$Camera3D.rotation.y = deg_to_rad(yaw)
 	if event is InputEventKey:
@@ -46,6 +47,7 @@ func _input(event) -> void:
 		if event.is_action_released("ui_page_up"):
 			flying = !flying
 		
+		## Show/hide mouse
 		if event.is_action_released("game_quit"):
 			mouseModeToggle = !mouseModeToggle
 			if mouseModeToggle:
@@ -60,50 +62,56 @@ func _exit_tree() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _physics_process(delta) -> void:
-	var aim = $Camera3D.global_transform.basis
-	var moveDir := Vector3.ZERO
-	if Input.is_action_pressed("move_forward"):
-		moveDir -= aim[2]
-	if Input.is_action_pressed("move_backward"):
-		moveDir += aim[2]
-	if Input.is_action_pressed("move_left"):
-		moveDir -= aim[0]
-	if Input.is_action_pressed("move_right"):
-		moveDir += aim[0]
-	
-	moveDir = moveDir.normalized()
-	isMoving = moveDir.length() > 0. # Reset flag for movement
-	
-	if flying: # Flying: up/down controls
-		if Input.is_action_pressed("jump"):
-			velocity.y += Accel
-		if Input.is_action_pressed("crouch"):
-			velocity.y -= Accel
-	else: # Not flying: gravity
-		velocity.y -= Gravity * delta
-		if Input.is_action_just_pressed("jump") && is_on_floor():
-			velocity.y += JumpForce
-	
-	# Calculate target position to move
-	var target := moveDir * speed
-	# Accelerate if moving
-	var accel := Deaccel
-	if isMoving:
-		accel = Accel
-	
-	if flying:
-		velocity = velocity.lerp(target, accel * delta)
-	else:
-		# Calculate the horizontal velocity to move toward the target
-		var hvel := velocity
-		hvel.y = 0.
+	if mouseModeToggle:
+		var aim = $Camera3D.global_transform.basis
+		var moveDir := Vector3.ZERO
+		if Input.is_action_pressed("move_forward"):
+			moveDir -= aim[2]
+		if Input.is_action_pressed("move_backward"):
+			moveDir += aim[2]
+		if Input.is_action_pressed("move_left"):
+			moveDir -= aim[0]
+		if Input.is_action_pressed("move_right"):
+			moveDir += aim[0]
 		
-		hvel = hvel.lerp(target, accel * delta)
-		velocity.x = hvel.x
-		velocity.z = hvel.z
-	
-	# Move the node
-	move_and_slide()
+		moveDir = moveDir.normalized()
+		isMoving = moveDir.length() > 0. # Reset flag for movement
+		
+		if flying: # Flying: up/down controls
+			if Input.is_action_pressed("jump"):
+				velocity.y += flySpeed / 2.
+			if Input.is_action_pressed("crouch"):
+				velocity.y -= flySpeed / 2.
+		else: # Not flying: gravity
+			velocity.y -= Gravity * delta
+			if Input.is_action_pressed("jump") && is_on_floor():
+				velocity.y += JumpForce
+		
+		# Calculate target position to move
+		var target: Vector3
+		if flying:
+			target = moveDir * flySpeed
+		else:
+			target = moveDir * walkSpeed
+		
+		# Accelerate if moving
+		var accel := Deaccel
+		if isMoving:
+			accel = Accel
+		
+		if flying:
+			velocity = velocity.lerp(target, accel * delta)
+		else:
+			# Calculate the horizontal velocity to move toward the target
+			var hvel := velocity
+			hvel.y = 0.
+			
+			hvel = hvel.lerp(target, accel * delta)
+			velocity.x = hvel.x
+			velocity.z = hvel.z
+		
+		# Move the node
+		move_and_slide()
 
 
 
